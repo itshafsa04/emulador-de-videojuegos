0990ee0fe8e9483462f543a2bfe306c2db675ea2
itshafsa04 <hafelbouslami@gmail.com>
2025-04-24 13:49:31 +0200

    New Commit

--- number.py
+++ number.py
@@ -1,27 +1,53 @@
 import random
 
-print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
+def main_menu():
+    print("Welcome to the Number Guessing Game!")
+    print("1. Start Game")
+    print("2. Exit")
+    choice = input("Please choose an option: ")
+    if choice == '1':
+        start_game()
+    elif choice == '2':
+        print("Thank you for playing! Goodbye!")
+    else:
+        print("Invalid choice. Please try again.")
+        main_menu()
 
-number_to_guess = random.randrange(100)
+def start_game():
+    print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
 
-chances = 7
+    number_to_guess = random.randrange(100)
+    chances = 7
+    guess_counter = 0
 
-guess_counter = 0
+    while guess_counter < chances:
+        guess_counter += 1
+        my_guess = int(input('Please Enter your Guess : '))
 
-while guess_counter < chances:
+        if my_guess == number_to_guess:
+            print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
+            break
+        elif guess_counter >= chances and my_guess != number_to_guess:
+            print(f'Oops sorry, The number is {number_to_guess} better luck next time')
+        elif my_guess > number_to_guess:
+            print('Your guess is higher ')
+        elif my_guess < number_to_guess:
+            print('Your guess is lesser')
 
-    guess_counter += 1
-    my_guess = int(input('Please Enter your Guess : '))
+    end_game()
 
-    if my_guess == number_to_guess:
-        print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
-        break
+def end_game():
+    print("Game Over!")
+    print("1. Play Again")
+    print("2. Return to Main Menu")
+    choice = input("Please choose an option: ")
+    if choice == '1':
+        start_game()
+    elif choice == '2':
+        main_menu()
+    else:
+        print("Invalid choice. Please try again.")
+        end_game()
 
-    elif guess_counter >= chances and my_guess != number_to_guess:
-        print(f'Oops sorry, The number is {number_to_guess} better luck next time')
-
-    elif my_guess > number_to_guess:
-        print('Your guess is higher ')
-
-    elif my_guess < number_to_guess:
-        print('Your guess is lesser')
+# Start the game by displaying the main menu
+main_menu()
