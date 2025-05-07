2f33da37aabcbcb41eec9d68539ddb9036a516f5
maikelasecas <michaeluribevalarezo@gmail.com>
2025-05-07 10:57:48 +0200

    New commit (new word-guessing2)

--- menu.py
+++ menu.py
@@ -25,8 +25,8 @@ def main_menu():
 
         elif choice == "2":
             print("\nIniciando el juego -Adivina la palabra...")
-            from wordguessing import seleccionar_modo as wordguessing_main # Importar el juego de adivina la palabra
-            wordguessing_main()
+            from wordguessing2 import iniciar_juego as wordguessing2_main # Importar el juego de adivina la palabra
+            wordguessing2_main()
 
         elif choice == "3":
             print("\nIniciando el juego -Ahorcado...")
--- wordguessing2.py
+++ wordguessing2.py
@@ -1,22 +1,32 @@
+# Libreria RandomWords para obtener palabras aleatorias
 from random_word import RandomWords
 
+# Funciones
 def iniciar_juego():
+
+    'esta función inicia el juego, pide el nombre del jugador, muestra las reglas, las pistas, obtiene una palabra aleatoria y detecta el modo de juego'
     name = input("Introduce tu nombre: ")
     print(f"\n¡Hola {name}, bienvenido al juego de adivinar la palabra!\n")
 
+    'aqui se obtiene una palabra aleatoria de la librería RandomWords'
     word = palabras()
+
+    'aqui se muestran las reglas del juego y las pistas'
     reglas()
-    pista_1, pista_2 = pistas(word)
+    pista_1, pista_2= pistas(word)
     print(f"\nPista 1: {pista_1}")
     print(f"Pista 2: {pista_2}")
 
+    'aqui se detecta en que modo esta jugando el jugador'
     modo_juego(word, name)
 
 def palabras():
+    'esta función obtiene una palabra aleatoria de la librería RandomWords'
     word = RandomWords().get_random_word()
     return word
 
 def reglas():
+    'esta función muestra las reglas del juego'
     print(f"\n\nAdivina la palabra: ")
     print("\n==Reglas del juego==")
     print("· Tienes 12 intentos para adivinar la palabra.")
@@ -28,11 +38,13 @@ def reglas():
     print("\n¡Buena suerte!\n")
 
 def pistas(word):
+    'esta función genera dos pistas sobre la palabra, una sobre la longitud y otra sobre la primera y última letra'
     pista_1 = f"La palabra comienza con la letra '{word[0]}' y termina con la letra '{word[-1]}'."
     pista_2 = f"La palabra contiene {len(word)} letras."
     return pista_1, pista_2
 
 def reiniciar_juego():
+    'esta función sirve para reiniciar el juego'
     while True:
         restart = input("\n¿Quieres jugar de nuevo? (si/no): ").strip().lower()
         if restart == 'si':
@@ -45,6 +57,8 @@ def reiniciar_juego():
             print("\nPor favor, responde con 'si' o 'no'.")
 
 def modo_juego(word, name):
+    'esta función detecta si el jugador está jugando en modo normal o en modo ruleta'
+    'estos son los turnos que tiene el jugador para adivinar la palabra'
     guesses = ''
     turns = 12
     while turns > 0:
@@ -65,10 +79,12 @@ def modo_juego(word, name):
 
         guess = input("\n\nAdivina una letra o la palabra completa: ").strip().lower()
 
+        'verifica si el jugador ha introducido un número o un caracter especial'
         if not guess.isalpha():
             print("\nEntrada no válida. Por favor, introduce una letra o una palabra.")
             continue
-
+        
+        'si el jugador ha introducido la misma letra, se le avisa'
         if len(guess) == 1:
             if guess in guesses:
                 print("\nYa has adivinado esa letra. Intenta con otra.")
@@ -76,13 +92,15 @@ def modo_juego(word, name):
 
             guesses += guess
 
+            'si el jugador introduce una letra que no está en la palabra, se le resta un intento'
             if guess not in word:
                 turns -= 1
                 print("\nIncorrecta")
                 print(f"Te quedan {turns} intentos.")
             else:
                 print("\nCorrecta")
-
+        
+        # en caso de que el jugador haya introducido una palabra completa compatible con la longitud de la palabra cambia el modo de juego
         elif len(guess) == len(word):
             modo_ruleta(word, guess, name)
             return
@@ -97,6 +115,7 @@ def modo_juego(word, name):
             return
 
 def modo_ruleta(word, guess, name):
+    'esta función es el modo ruleta, donde el jugador puede adivinar la palabra completa, si acierta gana, si no pierde. Solo tiene un intento'
     if guess == word:
         print(f"\n¡Felicidades {name}, has Ganado!")
         print(f"La palabra era: {word}")
